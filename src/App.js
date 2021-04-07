9c12237b1efbbc6cb6803ef9b29c8f6f4a579b97
vinor228 <eduardmortred@gmail.com>
2021-04-07 17:33:00 +0300

    first_changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
+// import "bootstrap/dist/css/bootstrap.css";
+// import "bootswatch/lux/bootstrap.css";
+import "bootswatch/dist/lux/bootstrap.min.css";
+import { Navbar, NavItem, Nav, Container, Row, Col } from "react-bootstrap";
+import React from "react";
+import WeatherDisplay from "./WatherDisplay/WeatherDisplay";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const PLACES = [
+    { name: "Palo Alto", zip: "94303" },
+    { name: "San Jose", zip: "94088" },
+    { name: "Santa Cruz", zip: "95062" },
+    { name: "Honolulu", zip: "96803" }
+];
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            activePlace: 0,
+        }
+    }
+
+    render() {
+        const activePlace = this.state.activePlace
+        return (
+            <div >
+                <Navbar>
+                    <Navbar.Brand>
+                        React Simple Weather App
+                    </Navbar.Brand>
+                </Navbar>
+                <Container>
+                    <Row>
+                        <Col md={4} sm={4}>
+                            <h3>Select a city</h3>
+                            <Nav
+                                bsStyle="pills"
+                                stacked
+                                activeKey={activePlace}
+                                onSelect={index => {
+                                    this.setState({ activePlace: index });
+                                }}
+                            >
+                                {PLACES.map((place, index) => (
+                                    <NavItem key={index} eventKey={index}>{place.name}</NavItem>
+                                ))}
+                            </Nav>
+                        </Col>
+                        <Col md={8} sm={8}>
+                            <WeatherDisplay key={activePlace} zip={PLACES[activePlace].zip} />
+                        </Col>
+                    </Row>
+                </Container>
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
---
+++ src/WatherDisplay/WeatherDisplay.js
@@ -0,0 +1,43 @@
+import React from "react";
+
+export default class WeatherDisplay extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            weatherData : null,
+        }
+    }
+
+    componentDidMount() {
+        const zip = this.props.zip
+        const URL = "http://api.openweathermap.org/data/2.5/weather?q=" +
+            zip +
+            "&appid=b1b35bba8b434a28a0be2a3e1071ae5b&units=imperial"
+        fetch(URL).then(res => res.json()).then(json => {
+            this.setState({weatherData: json})
+            // console.log(json)
+        })
+
+    }
+
+        const weatherData = this.state.weatherData
+        console.log(weatherData)
+        if (!weatherData){
+            return <div>Loading</div>;
+        }
+        const weather = weatherData.weather[0];
+        const iconUrl = "http://openweathermap.org/img/w/" + weather.icon + ".png";
+        return (
+            <div>
+                <h1>
+                    {weather.main} in {weatherData.name}
+                    <img src={iconUrl} alt={weatherData.description} />
+                </h1>
+                <p>Current: {weatherData.main.temp}°</p>
+                <p>High: {weatherData.main.temp_max}°</p>
+                <p>Low: {weatherData.main.temp_min}°</p>
+                <p>Wind Speed: {weatherData.wind.speed} mi/hr</p>
+            </div>
+        );
+    }
+}
